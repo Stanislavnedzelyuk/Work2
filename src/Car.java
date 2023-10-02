ca4d6dd14b55922303dad9d4408d00c52e4d37b7
Stanislav <stanislav.nedzelyuk@icloud.com>
2023-10-02 08:28:41 +0300

    Домашняя работа

---
+++ src/Car.java
@@ -0,0 +1,15 @@
+class Car extends Vehicle {
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    @Override
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    @Override
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
